3f775f446844f72e6d4267d4a752b3bd680df811
Lasith95 <126706472+Lasith95@users.noreply.github.com>
2026-03-16 04:02:33 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'staff-portal-cache-v7';
+// cache කරන්න ඕනෙ ප්‍රධාන දේවල් මෙතන තියෙන්න ඕනේ
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+// Service Worker එක Install වෙද්දී files cache කරගැනීම
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Offline වෙලාවකදී cache එකෙන් data ලබාදීම
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
